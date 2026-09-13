17587126f4c35e7c7a992f1458453698c86c40d2
Roan Carmelo G. Morgado <roancarmelomorgado@gmail.com>
2026-09-14 01:24:11 +0800

    Url change for registrations of accounts

--- ticket_system/customers/urls.py
+++ ticket_system/customers/urls.py
@@ -3,5 +3,5 @@ from .views import CustomersView, CustomersCreate
 
 urlpatterns = [
     path('customers/', CustomersView.as_view()),
-    path('customers/create/', CustomersCreate.as_view()),
+    path('register-customers/', CustomersCreate.as_view()),
 ]
--- ticket_system/technicians/urls.py
+++ ticket_system/technicians/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import TechnicianRegister, TechnicianLogin, TechnicianView
 urlpatterns = [
-    path('technicians/create/', TechnicianRegister.as_view()),
+    path('register-technicians/', TechnicianRegister.as_view()),
     path('login/', TechnicianLogin.as_view()),
     path('technicians/', TechnicianView.as_view()),
 ]
