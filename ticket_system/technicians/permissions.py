127b2db97f9755348a8f5703355402c3ba2f68d4
Roan Carmelo G. Morgado <roancarmelomorgado@gmail.com>
2026-09-14 01:21:40 +0800

    Url update for registration of roles

--- ticket_system/technicians/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class isAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_admin and request.user
